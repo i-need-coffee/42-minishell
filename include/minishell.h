be9a5dbb4ee585e15fdfae0c7de900f544863020
Shadya Jolliet <sjolliet@student.42.fr>
2026-04-17 15:57:43 +0200

    style: fix norm issues except missing headers

--- include/minishell.h
+++ include/minishell.h
@@ -1,10 +1,8 @@
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
 # include <libft.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,18 +2,17 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+	char	*line;
+
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	char	*line;
-
 	while (1)
 	{
 		line = readline("minishell > ");
 		if (!line)
-			break;
-		ft_printf("you written %s\n", line);
-		free(line);	
+			break ;
+		free(line);
 	}
 	return (0);
 }
