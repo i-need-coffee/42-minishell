b6c962ec6f125a8c58f20f48bc968cff704db2e9
Shadya Jolliet <sjolliet@student.42.fr>
2026-04-17 16:15:48 +0200

    feat: add tokenization enum and struct to header

--- include/minishell.h
+++ include/minishell.h
@@ -5,4 +5,23 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIR_IN,
+	TOKEN_REDIR_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+	TOKEN_EOF
+}	t_token_type;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	char			*value;
+	struct s_token	*prev;
+	struct s_token	*next;
+}	t_token;
+
 #endif
