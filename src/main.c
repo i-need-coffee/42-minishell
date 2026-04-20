f2c824f5862524dd0d7a2c3021d5a54cc45d4c8a
Shadya Jolliet <shadya.jolliet@outlook.com>
2026-04-20 20:01:59 +0200

    fix: remove space in readline function to make it more clear for user that the > is part of the name (minishell> instead of minishell >)

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int	main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		ft_bzero(&mini, sizeof(t_mini));
-		mini.input = readline("minishell > ");
+		mini.input = readline("minishell> ");
 		if (!mini.input)
 			break ;
 		tokenize_input(&mini);
