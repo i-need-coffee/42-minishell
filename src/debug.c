4c9d9e339e2472bbb3a5355734c151a3d945d9ce
Shadya Jolliet <shadya.jolliet@outlook.com>
2026-06-06 05:48:13 +0200

    fix: improve debgug function to print tokens

--- src/debug.c
+++ src/debug.c
@@ -4,23 +4,27 @@ void	print_tokens(t_token **root)
 {
 	t_token			*curr;
 	t_token_segment	*seg;
+	int				i;
 
 	curr = *root;
+	i = 0;
 	ft_printf("-- TOKENS START --\n");
 	while (curr->type != TOKEN_EOF)
 	{
-		ft_printf("TYPE: %d\n", curr->type);
+		ft_printf("\nTOKEN[%d]\n", i);
+		ft_printf("  TYPE: %d\n", curr->type);
 		if (curr->segments)
 		{
 			seg = curr->segments;
 			while (seg)
 			{
-				ft_printf("  SEG[quote=%d, value=%s]\n",
+				ft_printf("  SEG[quote: %d, value: %s]\n",
 					seg->quote_type, seg->value);
 				seg = seg->next;
 			}
 		}
+		i++;
 		curr = curr->next;
 	}
-	ft_printf("-- TOKENS END --\n");
+	ft_printf("\n-- TOKENS END --\n");
 }
