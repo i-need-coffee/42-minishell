c5c1a7fada77a12aa95f4520f1e6b91fcd161e6e
Shadya Jolliet <shadya.jolliet@outlook.com>
2026-06-11 15:45:00 +0200

    refractor: remove print_node function from tokenization as we already have a function in debug file to print tokens

--- src/tokenization/utils.c
+++ src/tokenization/utils.c
@@ -62,12 +62,3 @@ static t_token	*create_token(t_token_type type, char *value)
 	}
 	return (new_token);
 }
-
-void print_node(t_token *node)
-{
-	while(node)
-	{
-		ft_printf("type: %d, value: %s\n", node->type, node->value);
-		node = node->next;
-	}
-}
