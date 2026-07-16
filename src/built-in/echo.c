150011f6ac427cbb047f9dace75c5ea91e5dbac6
Shadya Jolliet <sjolliet@student.42.fr>
2026-07-16 22:31:11 +0200

    fix: improve echo function

--- src/built-in/echo.c
+++ src/built-in/echo.c
@@ -1,22 +1,59 @@
 #include "minishell.h"
 
+static int	is_n_flag(char *arg);
+static int	count_args(char **args);
+
 void	echo(char **args)
 {
 	int	has_newline;
 	int	i;
+	int	last_arg;
 
 	has_newline = 1;
 	i = 1;
-	if (args[1] && ft_strcmp(args[1], "-n") == 0)
+	if (args[1] && is_n_flag(args[1]))
 	{
 		has_newline = 0;
 		i = 2;
 	}
+	while (args[i] && is_n_flag(args[i]))
+		i++;
+	last_arg = count_args(args) - 1;
 	while (args[i])
 	{
 		write(STDOUT_FILENO, args[i], ft_strlen(args[i]));
+		if (i != last_arg)
+			write(STDOUT_FILENO, " ", 1);
 		i++;
 	}
 	if (has_newline)
 		write(STDOUT_FILENO, "\n", 1);
+}
+
+static int	is_n_flag(char *arg)
+{
+	int	i;
+
+	if (ft_strlen(arg) < 2)
+		return (0);
+	if (arg[0] != '-')
+		return (0);
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	count_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
 }
--- src/execution/execute_input.c
+++ src/execution/execute_input.c
@@ -4,7 +4,6 @@ static int	exec_in_parent(t_mini *mini, t_pipe_unit *cmd);
 static int	set_exec_error(t_mini *mini);
 static void	close_saved_fds(int stdin, int stdout);
 
-
 void	init_data(t_mini *mini)
 {
 	t_pipe_unit	*u1;
@@ -12,10 +11,13 @@ void	init_data(t_mini *mini)
 	// echo "hello, world!"
 	u1 = calloc(1, sizeof(t_pipe_unit));
 	u1->type = CMD;
-	u1->args = malloc(sizeof(char *) * 3);
+	u1->args = malloc(sizeof(char *) * 6);
 	u1->args[0] = ft_strdup("echo");
-	u1->args[1] = ft_strdup("hello, world!");
-	u1->args[2] = NULL;
+	u1->args[1] = ft_strdup("-nnn");
+	u1->args[2] = ft_strdup("-na");
+	u1->args[3] = ft_strdup("-n");
+	u1->args[4] = ft_strdup("hello, world!");
+	u1->args[5] = NULL;
 	u1->fd = -1;
 	u1->cmd_index = 0;
 	u1->next = NULL;
