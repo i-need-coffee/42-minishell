1bfab463f9b1fac9669578127094580161c20ebf
Shadya Jolliet <sjolliet@student.42.fr>
2026-07-31 16:54:49 +0200

    fix: change exit code for env to use the same as on other built-ins for too many args

--- src/built-in/env.c
+++ src/built-in/env.c
@@ -9,7 +9,7 @@ int	env(char **envp, char **args)
 	int		i;
 
 	if (count_args(args) > 1)
-		return (print_error(ERR_ENV_ARGS_NUM), 125);
+		return (print_error(ERR_ENV_ARGS_NUM), 1);
 	i = 0;
 	while (envp[i])
 	{
