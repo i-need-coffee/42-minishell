10146c2d64eea8d970fe29400663b4db20957409
Juliette Turrel <jturrel@c1r5s5.42lausanne.ch>
2026-08-06 18:29:37 +0200

    feat : do not expend the file when token heredoc

--- src/parsing/parse_redirection_token.c
+++ src/parsing/parse_redirection_token.c
@@ -55,7 +55,7 @@ int	quote_part(char **str, int *i, t_mini *mini)
 	return (1);
 }
 
-char	*get_file_name(t_mini *mini, char **next_value, char **str)
+char	*get_file_name(t_mini *mini, char **next_value, char **str, t_token *current)
 {
 	int		i;
 	char	*tmp_str;
@@ -72,7 +72,7 @@ char	*get_file_name(t_mini *mini, char **next_value, char **str)
 				return (NULL);
 			continue ;
 		}
-//		if (tmp_str[i] == '$' && current->type != TOKEN_HEREDOC)
+		if (tmp_str[i] == '$' && current->type != TOKEN_HEREDOC)
 		{
 			if (!(wrapper_handle_dollar(&tmp_str, &i, mini)))
 				return (NULL);
@@ -101,7 +101,7 @@ int	uptade_unit_struct_redir(t_mini *mini, t_token *current, t_unit_type type,
 	current_node = *head;
 	while (current_node->next)
 		current_node = current_node->next;
-	file_name = get_file_name(mini, &current->next->value, &str);
+	file_name = get_file_name(mini, &current->next->value, &str, current);
 	if (!file_name)
 		return (free_and_null(&str), 0);
 	if (!(create_redirection_node(head, current->next, file_name)))
